b22b4d7bd88fd28dfb31a076eb4eebb99f07f3a3
Cong Le <cong.le@vtijs.com>
2024-05-21 15:52:02 +0700

    Blog App Category Component Design

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./featured.module.css"
+import Image from "next/image"
 
 const Featured = () => {
     return (
@@ -8,9 +9,19 @@ const Featured = () => {
                 <b>Hey, Cong Le here!</b> Discover my stories and creative ideas.</h1>
                 <div className={styles.post}>
                     <div className={styles.imgContainer}>
-                        <Image src="/p1.png" alt="" fill />
+                        <Image src="/p1.jpeg" alt="" fill className={styles.image} />
                     </div>
-                    <div className={styles.imgContainer}></div>
+                    <div className={styles.textContainer}>
+                        <h1 className={styles.postTitle}>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h1>
+                        <p className={styles.postDesc}>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            Quisquam, quidem.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            Quisquam, quidem.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe, quas impedit. Magni eius deserunt provident assumenda laboriosam quidem at, maxime debitis similique et. Non cupiditate minima, iste laboriosam similique labore.
+                        </p>
+                        <button className={styles.button}>Read More</button>
+                    </div>
                 </div>
         </div>
     )
