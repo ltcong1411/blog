ef1356ae61088a6a33b7322b95367da36e691f93
Cong Le <cong.le@vtijs.com>
2024-05-22 14:23:49 +0700

    Blog Post Card Component Design

---
+++ src/components/card/Card.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import styles from "./card.module.css";
+import Image from "next/image";
+import Link from "next/link";
+
+const Card = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.imageContainer}>
+        <Image src="/p1.jpeg" alt="" fill className={styles.image} />
+      </div>
+      <div className={styles.textContainer}>
+        <div className={styles.detail}>
+            <span className={styles.date}>22.05.2024 - </span>
+            <span className={styles.category}>CULTURE</span>
+        </div>
+        <Link href="/">
+        <h1>Lorem ipsum dolor sit amet</h1>
+        </Link>
+        <p className={styles.desc}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+          quidem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque obcaecati quisquam quia, tempore dolorum perferendis maxime ad iure similique iusto magni aliquam totam recusandae laudantium, omnis explicabo blanditiis nobis iste....
+        </p>
+        <Link href="/" className={styles.link}>Read More</Link>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
