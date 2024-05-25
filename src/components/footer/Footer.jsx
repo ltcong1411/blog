722b3c8bece1a1f0b4da9772f9f7955b29f4ee98
ltcong1411 <ltcong1411@gmail.com>
2024-05-25 22:19:36 +0700

    Footer Component Design

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,50 @@
 import React from "react";
 import styles from "./footer.module.css"
+import Image from "next/image"
+import Link from "next/link"
 
 const Footer = () => {
     return (
-        <div className={styles.container}>Footer</div>
+        <div className={styles.container}>
+            <div className={styles.info}>
+                <div className={styles.logo}>
+                    <Image src="/logo.png" alt="congle blog" width={50} height={50} />
+                    <h1 className={styles.logoText}>CongLeBlog</h1>
+                </div>
+                <p className={styles.desc}>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt suscipit quaerat itaque, aut a nulla! Numquam laboriosam et labore saepe maiores, iure voluptatem asperiores, assumenda explicabo distinctio tempore recusandae at!
+                </p>
+                <div className={styles.icons}>
+                    <Image src="/facebook.png" alt="facebook" width={18} height={18} />
+                    <Image src="/instagram.png" alt="instagram" width={18} height={18} />
+                    <Image src="/tiktok.png" alt="tiktok" width={18} height={18} />
+                    <Image src="/youtube.png" alt="youtube" width={18} height={18} />
+                </div>
+            </div>
+            <div className={styles.links}>
+                <div className={styles.link}>
+                    <span className={styles.linkTitle}>Links</span>
+                    <Link href="/">Homepage</Link>
+                    <Link href="/">Blog</Link>
+                    <Link href="/">About</Link>
+                    <Link href="/">Contact</Link>
+                </div>
+                <div className={styles.link}>
+                    <span className={styles.linkTitle}>Tags</span>
+                    <Link href="/">Style</Link>
+                    <Link href="/">Fashion</Link>
+                    <Link href="/">Coding</Link>
+                    <Link href="/">Travel</Link>
+                </div>
+                <div className={styles.link}>
+                    <span className={styles.linkTitle}>Social</span>
+                    <Link href="/">Facebook</Link>
+                    <Link href="/">Instagram</Link>
+                    <Link href="/">Tiktok</Link>
+                    <Link href="/">Youtube</Link>
+                </div>
+            </div>
+        </div>
     )
 }
 
