7a89c586cfe100d940614bc895a59b082b26b943
ltcong1411 <ltcong1411@gmail.com>
2024-05-26 21:49:37 +0700

    Blog Category Page Design

---
+++ src/app/blog/page.jsx
@@ -0,0 +1,17 @@
+import styles from "./blog.module.css"
+import CardList from "@/components/cardList/CardList";
+import Menu from "@/components/menu/Menu";
+
+const Blog = () => {
+    return (
+        <div className={styles.container}>
+            <h1 className={styles.title}>Style Blog</h1>
+            <div className={styles.content}>
+                <CardList />
+                <Menu />
+            </div>
+        </div>
+    )
+}
+
+export default Blog
