dd6358b76d34548d2c380026c747825c79d4d1a3
Cong Le <cong.le@vtijs.com>
2024-06-03 13:39:21 +0700

    Next.js How to Sign in with Google - 1

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,15 +1,24 @@
-import styles from "./loginPage.module.css"
+"use client";
+
+import styles from "./loginPage.module.css";
+import { signIn, useSession } from "next-auth/react";
 
 const LoginPage = () => {
-    return (
-        <div className={styles.container}>
-            <div className={styles.wrapper}>
-                <div className={styles.socialButton}>Sign in with Google</div>
-                <div className={styles.socialButton}>Sign in with Github</div>
-                <div className={styles.socialButton}>Sign in with Facebook</div>
-            </div>
-        </div>
-    )
-}
+  const { data, status } = useSession();
 
-export default LoginPage
+  console.log(data, status);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <div className={styles.socialButton} onClick={() => signIn("google")}>
+          Sign in with Google
+        </div>
+        <div className={styles.socialButton}>Sign in with Github</div>
+        <div className={styles.socialButton}>Sign in with Facebook</div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
