42cd14f9a7482c1b47c50d10ed12403de92fdb9c
Cong Le <cong.le@vtijs.com>
2024-06-04 16:35:14 +0700

    Next.js Send a Query to API (Fetch Post by Category)

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -5,20 +5,24 @@ export const GET = async (req) => {
   const { searchParams } = new URL(req.url);
 
   const page = searchParams.get("page");
+  const cat = searchParams.get("cat");
 
   const POST_PER_PAGE = 2;
 
   const query = {
     take: POST_PER_PAGE,
     skip: (parseInt(page) - 1) * POST_PER_PAGE,
-  }
+    where: {
+      ...(cat && { catSlug: cat }),
+    },
+  };
 
   try {
     const [posts, count] = await prisma.$transaction([
       prisma.post.findMany(query),
-      prisma.post.count(),
+      prisma.post.count({ where: query.where }),
     ]);
-    return new NextResponse(JSON.stringify({posts, count}), { status: 200 });
+    return new NextResponse(JSON.stringify({ posts, count }), { status: 200 });
   } catch (err) {
     console.log(err);
     return new NextResponse(
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -2,16 +2,19 @@ import styles from "./blog.module.css"
 import CardList from "@/components/cardList/CardList";
 import Menu from "@/components/menu/Menu";
 
-const Blog = () => {
+const BlogPage = ({searchParams}) => {
+    const page = parseInt(searchParams.page) || 1;
+    const { cat } = searchParams
+
     return (
         <div className={styles.container}>
             <h1 className={styles.title}>Style Blog</h1>
             <div className={styles.content}>
-                <CardList />
+                <CardList page={page} cat={cat}/>
                 <Menu />
             </div>
         </div>
     )
 }
 
-export default Blog
+export default BlogPage
