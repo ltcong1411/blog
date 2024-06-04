12c3bb9feca9a1e1b4584293ec113a5793277fdb
ltcong1411 <ltcong1411@gmail.com>
2024-06-04 22:07:25 +0700

    Fetch a Single Post with the User Information

--- src/app/posts/[slug]/page.jsx
+++ src/app/posts/[slug]/page.jsx
@@ -3,43 +3,47 @@ import Menu from "@/components/menu/Menu";
 import Comments from "@/components/comments/Comments";
 import Image from "next/image";
 
-const SinglePage = () => {
+const getData = async (slug) => {
+    const res = await fetch(
+        `http://localhost:3000/api/posts/${slug}`,
+        {
+            cache: "no-store",
+        }
+    );
+
+    if (!res.ok) {
+        throw new Error("Failed to fetch data");
+    }
+
+    return res.json();
+};
+
+const SinglePage = async ({ params }) => {
+    const { slug } = params;
+    const data = await getData(slug);
+
     return (
         <div className={styles.container}>
             <div className={styles.infoContainer}>
                 <div className={styles.textContainer}>
-                    <h1 className={styles.title}>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</h1>
+                    <h1 className={styles.title}>{data?.title}</h1>
                     <div className={styles.user}>
-                        <div className={styles.userImageContainer}>
-                            <Image src="/p1.jpeg" alt="" fill className={styles.avatar} />
-                        </div>
+                        {data?.user?.image && (<div className={styles.userImageContainer}>
+                            <Image src={data?.user?.image} alt="" fill className={styles.avatar} />
+                        </div>)}
                         <div className={styles.userTextContainer}>
-                            <span className={styles.username}>Cong Le</span>
-                            <span className={styles.date}>26.05.2024</span>
+                            <span className={styles.username}>{data?.user?.name}</span>
+                            <span className={styles.date}>{data?.createdAt.substring(0, 10)}</span>
                         </div>
                     </div>
                 </div>
-                <div className={styles.imageContainer}>
-                    <Image src="/p1.jpeg" alt="" fill className={styles.image} />
-                </div>
+                {data?.img && (<div className={styles.imageContainer}>
+                    <Image src={data?.img} alt="" fill className={styles.image} />
+                </div>)}
             </div>
             <div className={styles.content}>
                 <div className={styles.post}>
-                    <div className={styles.description}>
-                        <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero recusandae odit animi voluptatibus consequatur beatae tenetur amet velit deserunt dicta, ad excepturi doloremque. Ut placeat, a illum totam eaque labore!
-                        </p>
-                        <h2>Lorem ipsum</h2>
-                        <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero recusandae odit animi voluptatibus consequatur beatae tenetur amet velit deserunt dicta, ad excepturi doloremque. Ut placeat, a illum totam eaque labore!
-                        </p>
-                        <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero recusandae odit animi voluptatibus consequatur beatae tenetur amet velit deserunt dicta, ad excepturi doloremque. Ut placeat, a illum totam eaque labore!
-                        </p>
-                        <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero recusandae odit animi voluptatibus consequatur beatae tenetur amet velit deserunt dicta, ad excepturi doloremque. Ut placeat, a illum totam eaque labore!
-                        </p>
-                    </div>
+                    <div className={styles.description} dangerouslySetInnerHTML={{ __html: data?.desc }} />
                     <div className={styles.comments}>
                         <Comments />
                     </div>
