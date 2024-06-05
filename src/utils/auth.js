a6b057d2e452ac0617b391b56e9e5d70384fb077
Cong Le <cong.le@vtijs.com>
2024-06-05 15:34:51 +0700

    Create a Comment

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -2,6 +2,7 @@ import GoogleProvider from "next-auth/providers/google";
 import GithubProvider from "next-auth/providers/github";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma from "./connect";
+import { getServerSession } from "next-auth";
 
 export const authOptions = {
     adapter: PrismaAdapter(prisma),
@@ -15,4 +16,6 @@ export const authOptions = {
             clientSecret: process.env.GITHUB_SECRET,
         }),
     ],
-}
+};
+
+export const getAuthSession = () => getServerSession(authOptions);
